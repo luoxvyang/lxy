579651a76016c564c5313241cd8ee95fdb5b4486
luoxvyang <78293712+luoxvyang@users.noreply.github.com>
2022-08-21 20:32:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def get_timenow():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
   weather = res['data']['list'][0]
-  return weather['lastUpdateTime'])
+  return weather.lastUpdateTime
 
 def get_city():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
