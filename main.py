2f1a0e23d28c4f558e8d80cba446def0e3f87499
luoxvyang <78293712+luoxvyang@users.noreply.github.com>
2022-08-21 21:40:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import random
 
 
 
-today = datetime.now(date, "%Y-%m-%d")
+today = datetime.now()
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
 birthday = os.environ['BIRTHDAY']
