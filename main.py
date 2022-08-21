2460d49aca3a752a64de38e3bf1d835b23cce107
luoxvyang <78293712+luoxvyang@users.noreply.github.com>
2022-08-21 20:35:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -36,11 +36,11 @@ def get_zuigao():
   weather = res['data']['list'][0]
   return weather['high'], math.floor(weather['high'])
 
-def get_timenow():
+def str(get_timenow()):
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
   weather = res['data']['list'][0]
-  return weather.lastUpdateTime
+  return weather['lastUpdateTime'], math.floor(weather['lastUpdateTime'])
 
 def get_city():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
