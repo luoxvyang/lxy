20b9933c0916ded1d2c315aa63921def2ffdebac
luoxvyang <78293712+luoxvyang@users.noreply.github.com>
2022-08-21 21:22:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from wechatpy.client.api import WeChatMessage, WeChatTemplate
 import requests
 import os
 import random
+import datetime
+
 
 today = datetime.now()
 start_date = os.environ['START_DATE']
